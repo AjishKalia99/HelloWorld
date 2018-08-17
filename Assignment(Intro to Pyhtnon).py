8c101e545eace38c03a46046469823569963e665
AjishKalia99 <ajishkalia123@gmail.com>
2018-08-17 15:34:41 +0530

    Assignment of Lec 2

---
+++ Assignment(Intro to Pyhtnon).py
@@ -0,0 +1,29 @@
+#Question 1
+print("Hello Python")
+
+#Question 2
+print("Ajish"+"Kalia")
+
+#Question 3
+x=input("Enter X ")
+y=input("Enter Y ")
+z=input("Enter Z ")
+print("The value of X is "+x)
+print("The value of Y is "+y)
+print("The value of Z is "+z)
+
+#Question 4
+print("Lets Get Started")
+
+#Question 5
+s="AcadView"
+f="Python"
+c=5000
+print('s = "%s"'%(s))
+print('course = "%s"'%(f))
+print('fees = %d'%(c))
+
+#Question 6
+Rad=int(input("Enter the radius of the circle "))
+Ar=3.14*Rad*Rad
+print(Ar)
